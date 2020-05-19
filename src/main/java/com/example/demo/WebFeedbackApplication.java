6d1d6471cdcae28aed23291aabb151266b2126a1
Vo Van Tam <vovantam.dev@gmail.com>
2020-05-19 16:02:23 +0700

    Initialized

---
+++ src/main/java/com/example/demo/WebFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFeedbackApplication.class, args);
+	}
+
+}
